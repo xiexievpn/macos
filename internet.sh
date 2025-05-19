72fcfe40af7293ac5dcad36b955b40ec7db096c4
hahahs <zhaifangyuan2@gmail.com>
2025-05-19 08:21:25 +0800

    Update internet.sh

--- internet.sh
+++ internet.sh
@@ -2,7 +2,9 @@
 
 cd "$(dirname "$0")"
 
-./xray -c ./config.json > /dev/null 2>&1 &
+CONFIG_PATH="$HOME/Library/Application Support/XieXieVPN/config.json"
+
+./xray -c "$CONFIG_PATH" > /dev/null 2>&1 &
 
 sudo networksetup -setwebproxy "Wi-Fi" 127.0.0.1 1080
 sudo networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 1080
