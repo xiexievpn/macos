542eadaa85b6265dc943fc88dd0d16c6d433795d
hahahs <zhaifangyuan2@gmail.com>
2026-01-09 17:52:23 +0800

    Update close.sh

--- close.sh
+++ close.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-
 cd "$(dirname "$0")"
 
 pkill xray
 
-sudo networksetup -setwebproxystate "Wi-Fi" off
-sudo networksetup -setsecurewebproxystate "Wi-Fi" off
-sudo networksetup -setsocksfirewallproxystate "Wi-Fi" off
+# 同样的自动检测逻辑，确保关对网卡
+DEFAULT_DEV=$(route -n get default | grep 'interface:' | awk '{print $2}')
+if [ -z "$DEFAULT_DEV" ]; then
+    SERVICE_NAME="Wi-Fi"
+else
+    SERVICE_NAME=$(networksetup -listallhardwareports | grep -B 1 "$DEFAULT_DEV" | grep "Hardware Port" | cut -d ": " -f 2)
+fi
+if [ -z "$SERVICE_NAME" ]; then SERVICE_NAME="Wi-Fi"; fi
+
+networksetup -setwebproxystate "$SERVICE_NAME" off
+networksetup -setsecurewebproxystate "$SERVICE_NAME" off
+networksetup -setsocksfirewallproxystate "$SERVICE_NAME" off
 
 exit 0
